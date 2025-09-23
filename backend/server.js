ed95a192da07b1af12aacbe08a0d42efc6cee533
ahmed <ahmedakheirallah@gmail.com>
2025-09-23 15:09:41 +0400

    reapplied changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { Server } from "socket.io";
+import http from "http";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
@@ -8,6 +10,10 @@ dotenv.config();
 
 // Intializes express
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {origin: "*"},
+})
 
 app.use(express.json()); // Parses JSON
 app.use(helmet()); // Helmet is a secure middle ware that set additional security headers
@@ -20,6 +26,13 @@ app.use("/api/clues", cluesRoutes);
 // This checks the .env file and uses it to broadcast the API on a sepcific port
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+    console.log("New user connected:", socket.id);
+    socket.on("disconnect", (reason) => {
+        console.log("User disconnected:", socket.id, reason)
+    })
+})
+
+server.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 })
